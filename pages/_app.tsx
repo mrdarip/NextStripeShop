1c76b3a3e7f67955ae9dafe9d936b84d0ee9629a
mrdarip <darioelas2005@gmail.com>
2025-01-26 01:09:45 +0100

    installed vercel/speed-insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import { CartProvider } from '../context/cartContext';
 import '../styles/globals.css';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
