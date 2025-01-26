639c8e00768b94ee9a314f3987df46f01c5545dd
mrdarip <darioelas2005@gmail.com>
2025-01-26 20:19:05 +0100

    implemented vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import { CartProvider } from '../context/cartContext';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react";
 import Layout from '../components/Layout';
 import '../styles/globals.css';
 
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <CartProvider>
       <SpeedInsights />
+      <Analytics/>
       <Layout>
         <Component {...pageProps} />
       </Layout>
