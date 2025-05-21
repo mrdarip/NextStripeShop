8eb39c8c9cf6c71ee435e0863f1c0134d2fdabff
mrdarip <darioelas2005@gmail.com>
2025-05-21 13:48:53 +0200

    feat: limit countries to ship to and add shipping rates programatically

--- lib/serverActions.ts
+++ lib/serverActions.ts
@@ -1,11 +1,11 @@
-'use server';
-import Stripe from 'stripe';
+"use server";
+import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 /**
  * Retrieves a session from Stripe
- * 
+ *
  * @param session_id - The ID of the session to retrieve
  * @returns The session object as a plain object
  */
@@ -15,41 +15,66 @@ export async function getSession(session_id: string) {
       const session = await stripe.checkout.sessions.retrieve(session_id);
       return JSON.parse(JSON.stringify(session)); // Convert to plain object
     } else {
-      throw new Error('Invalid session ID');
+      throw new Error("Invalid session ID");
     }
   } catch (err) {
     if (err instanceof Error) {
       throw new Error(err.message);
     }
-    throw new Error('An unknown error occurred');
+    throw new Error("An unknown error occurred");
   }
 }
 
 /**
  * Creates a checkout session with Stripe
- * 
+ *
  * @param cart - Items in the cart
  * @param origin - The origin URL for success and cancel
  * @returns The session ID of the created session
  */
 export async function createCheckoutSession(cart: any, origin: string) {
   try {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
+    const shippingRates = await stripe.shippingRates.list({
+      limit: 10,
+      active: true,
+    });
+
+    console.log(JSON.stringify({
+      payment_method_types: ["card"],
       line_items: cart.map((item: any) => ({
         price: item.id,
         quantity: item.quantity,
       })),
-      mode: 'payment',
+      shipping_options: shippingRates.data.map((rate) => ({
+        shipping_rate: rate.id,
+      })),
+      mode: "payment",
+      success_url: `${origin}/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${origin}/cancel`,
+    }));
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: cart.map((item: any) => ({
+        price: item.id,
+        quantity: item.quantity,
+      })),
+      shipping_options: shippingRates.data.map((rate) => ({
+        shipping_rate: rate.id,
+      })),
+       shipping_address_collection: {
+        allowed_countries: ['ES', 'PT'],
+      },
+      mode: "payment",
       success_url: `${origin}/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${origin}/cancel`,
     });
-    
+
     return session.id;
   } catch (err) {
     if (err instanceof Error) {
       throw new Error(err.message);
     }
-    throw new Error('An unknown error occurred');
+    throw new Error("An unknown error occurred");
   }
 }
