44db1cb69c4fddb87a48b8d9057ba6fad6f9bb8a
mrdarip <darioelas2005@gmail.com>
2025-06-15 01:00:32 +0200

    style: replace img with next/image for improved image handling in Banner component

--- app/components/banner/Banner.tsx
+++ app/components/banner/Banner.tsx
@@ -1,5 +1,6 @@
 import styles from './Banner.module.css';
+import Image from 'next/image';
 
 export default function Banner() {
-  return <img className={styles.bannerImage} src="/images/banner.jpg" alt="Store Banner" />;
+  return <Image width={1920} height={1080} className={styles.bannerImage} src="/images/banner.jpg" alt="Store Banner" />;
 }
